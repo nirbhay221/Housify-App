ae6cd8049bb2b1b9ce0e73dff36cf6da869bd2ab
Aman Ahmed <aman.ahmed97@gmail.com>
2023-12-09 21:49:42 -0500

    property view info image

--- app/src/main/java/com/example/housify/propertyInfoActivity.kt
+++ app/src/main/java/com/example/housify/propertyInfoActivity.kt
@@ -1,10 +1,16 @@
 package com.example.housify
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.housify.Models.messageModel
 import com.example.housify.Models.propertyModel
 import com.google.firebase.auth.FirebaseAuth
@@ -17,16 +23,19 @@ import com.google.firebase.firestore.QuerySnapshot
 
 class propertyInfoActivity : AppCompatActivity() {
 
-    private lateinit var propertyLocation:TextView
-    private lateinit var propertyName:TextView
-    private lateinit var propertyUid:TextView
-    private lateinit var addUserToCurrentUserCollection:Button
-    private lateinit var propUid:String
+    private lateinit var propertyLocation: TextView
+    private lateinit var propertyName: TextView
+//    private lateinit var propertyImages: View
+    private lateinit var propertyImages: ImageView
+    private lateinit var propertyUid: TextView
+    private lateinit var addUserToCurrentUserCollection: Button
+    private lateinit var propUid: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_info)
         propertyLocation = findViewById(R.id.propertyLocationInfo)
         propertyName = findViewById(R.id.propertyNameInfo)
+        propertyImages = findViewById<ImageView>(R.id.propertyImages)
         propertyUid = findViewById(R.id.propertyUidInfo)
         addUserToCurrentUserCollection = findViewById(R.id.chatWithUser)
 
@@ -38,30 +47,51 @@ class propertyInfoActivity : AppCompatActivity() {
             propertyName.text = propName
             propertyLocation.text = propLocation
             propertyUid.text = propUid
-            if (propUid != null) {
-                getUserNameFromUid(propUid)
+            Toast.makeText(this, propUid,Toast.LENGTH_LONG).show()
+
+            propUid?.let { propertyUid ->
+                FirebaseFirestore.getInstance().collection("Properties").document(propUid).get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            var userImageBase64 = document.getString("userPropertyImages")
+
+                            if (!userImageBase64.isNullOrBlank()) {
+                                var decodedImage = decodeBase64ToBitmap(userImageBase64)
+                                propertyImages.setImageBitmap(decodedImage)
+                            }
+                        } else {
+                            Log.d("Property View Info", "Property details not found")
+                        }
+
+                    }.addOnFailureListener { exception ->
+                        Log.e("Property View Info", "Error getting property details", exception)
+                    }
+
+                if (propUid != null) {
+                    getUserNameFromUid(propUid)
+                }
+            }
+            addUserToCurrentUserCollection.setOnClickListener {
+                var fireStore = FirebaseFirestore.getInstance()
+                var currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
+                var selectedUserUid = propUid
+                if (currentUserUid != null && selectedUserUid != null) {
+                    addToChatCluster(currentUserUid, selectedUserUid)
+                    createChatDocument(currentUserUid, selectedUserUid)
+                }
+
+
             }
         }
-        addUserToCurrentUserCollection.setOnClickListener{
-            var fireStore = FirebaseFirestore.getInstance()
-            var currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
-            var selectedUserUid = propUid
-            if(currentUserUid != null && selectedUserUid != null){
-                addToChatCluster(currentUserUid,selectedUserUid)
-                createChatDocument(currentUserUid,selectedUserUid)
-        }
-
-
-}
     }
 
-    private fun createChatDocument(currentUserUid: String, selectedUserUid: String) {
-        var chatCollection = FirebaseFirestore.getInstance().collection("ChatsCollection")
-        var chatData = hashMapOf(
-            "UserParticipants" to arrayListOf(currentUserUid,selectedUserUid),
-            "messageUserUid" to getUniqueMessageUserUid(currentUserUid,selectedUserUid),
-            "timestamp" to FieldValue.serverTimestamp()
-        )
+        private fun createChatDocument(currentUserUid: String, selectedUserUid: String) {
+            var chatCollection = FirebaseFirestore.getInstance().collection("ChatsCollection")
+            var chatData = hashMapOf(
+                "UserParticipants" to arrayListOf(currentUserUid, selectedUserUid),
+                "messageUserUid" to getUniqueMessageUserUid(currentUserUid, selectedUserUid),
+                "timestamp" to FieldValue.serverTimestamp()
+            )
 //        chatCollection.add(chatData).addOnSuccessListener {
 //            document-> Log.d("propertyInfoActivity","Chat Document created")
 //            chatCollection.document(document.id).update("UserParticipants",FieldValue.arrayUnion(selectedUserUid))
@@ -79,74 +109,100 @@ class propertyInfoActivity : AppCompatActivity() {
 //        }.addOnFailureListener{exception ->
 //            Log.e("propertyInfoActivity","User add failure",exception)}
 
-        chatCollection.whereArrayContains("UserParticipants",currentUserUid)
-            .whereArrayContains("UserParticipants",selectedUserUid)
-            .get()
-            .addOnSuccessListener { documents ->
-                if(documents.isEmpty){
-                    chatCollection.add(chatData)
-                        .addOnSuccessListener { document ->
-                            Log.d("propertyInfoActivity","Chat Document Created")
-                            val messageUserUid = chatData["messageUserUid"].toString()
-                            chatCollection.document(document.id)
-                                .update("UserParticipants",FieldValue.arrayUnion(selectedUserUid,currentUserUid))
-                                .addOnSuccessListener { Log.d("propertyInfoActivity","User added to chat successfully") }
-                                .addOnFailureListener{ exception ->
-                                    Log.e("propertyInfoActivity","User add failure",exception)
-                                }
-                        }
+            chatCollection.whereArrayContains("UserParticipants", currentUserUid)
+                .whereArrayContains("UserParticipants", selectedUserUid)
+                .get()
+                .addOnSuccessListener { documents ->
+                    if (documents.isEmpty) {
+                        chatCollection.add(chatData)
+                            .addOnSuccessListener { document ->
+                                Log.d("propertyInfoActivity", "Chat Document Created")
+                                val messageUserUid = chatData["messageUserUid"].toString()
+                                chatCollection.document(document.id)
+                                    .update(
+                                        "UserParticipants",
+                                        FieldValue.arrayUnion(selectedUserUid, currentUserUid)
+                                    )
+                                    .addOnSuccessListener {
+                                        Log.d(
+                                            "propertyInfoActivity",
+                                            "User added to chat successfully"
+                                        )
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.e("propertyInfoActivity", "User add failure", exception)
+                                    }
+                            }
+                    } else {
+                        Log.d("propertyInfoActivity", "Chat Document Already Exists")
+
+                    }
+                }.addOnFailureListener { exception ->
+                    Log.e("propertyInfoActivity", "Error checking chat document")
                 }
-                else{
-                    Log.d("propertyInfoActivity","Chat Document Already Exists")
+        }
 
-                }
-            }.addOnFailureListener{exception ->
-                Log.e("propertyInfoActivity","Error checking chat document")
-            }
-    }
+        private fun getUniqueMessageUserUid(
+            currentUserUid: String,
+            selectedUserUid: String
+        ): String {
+            return "${currentUserUid}_${selectedUserUid}_${System.currentTimeMillis()}"
+        }
 
-    private fun getUniqueMessageUserUid(currentUserUid: String,selectedUserUid: String): String {
-        return "${currentUserUid}_${selectedUserUid}_${System.currentTimeMillis()}"
-    }
-
-    private fun getUserNameFromUid(uid:String){
-        val firestore = FirebaseFirestore.getInstance()
-        val usersCollection = firestore.collection("User")
-        if(uid != null){
-            usersCollection.document(uid).get().addOnSuccessListener {
-                documentSnapshot ->
-                if(documentSnapshot.exists()){
-                    val userFirstName = documentSnapshot.getString("firstName")
-                    val userLastName = documentSnapshot.getString("lastName")
+        private fun getUserNameFromUid(uid: String) {
+            val firestore = FirebaseFirestore.getInstance()
+            val usersCollection = firestore.collection("User")
+            if (uid != null) {
+                usersCollection.document(uid).get().addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val userFirstName = documentSnapshot.getString("firstName")
+                        val userLastName = documentSnapshot.getString("lastName")
 //                    propertyUid.text = userFirstName + " "+ userLastName
-                    propertyUid.text = userFirstName
-                }else{
+                        propertyUid.text = userFirstName
+                    } else {
 
+                    }
+                }.addOnFailureListener { exception ->
+                    Log.e("propertyInfoActivity", "Error getting User name", exception)
                 }
-            }.addOnFailureListener{exception ->
-                Log.e("propertyInfoActivity","Error getting User name",exception)
             }
         }
+
+    private fun addToChatCluster(currentUserUid: String, selectedUserUid: String) {
+        var chatUserClusterCollection =
+            FirebaseFirestore.getInstance().collection("ChatUserCluster")
+        chatUserClusterCollection.document(currentUserUid)
+            .update("connectedUsers", FieldValue.arrayUnion(selectedUserUid))
+            .addOnSuccessListener {
+                Log.d(
+                    "propertyInfoActivity",
+                    "User $selectedUserUid added to the following $currentUserUid cluster successfully."
+                )
+                chatUserClusterCollection.document(selectedUserUid)
+                    .update("connectedUsers", FieldValue.arrayUnion(currentUserUid))
+                    .addOnSuccessListener {
+                        Log.d(
+                            "propertyInfoActivity",
+                            "User $currentUserUid added to the following $selectedUserUid cluster successfully."
+                        )
+                    }.addOnFailureListener { exception ->
+                        Log.e(
+                            "propertyInfoActivity",
+                            "Errorr in adding user to the chat cluster",
+                            exception
+                        )
+                    }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("propertyInfoActivity", "Error in adding user to the chat cluster", exception)
+            }
+    }
+    private fun decodeBase64ToBitmap(base64:String): Bitmap {
+        var decodeByteArray = Base64.decode(base64, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodeByteArray,0,decodeByteArray.size)
     }
 }
 
-    private fun addToChatCluster(currentUserUid: String, selectedUserUid: String) {
-        var chatUserClusterCollection = FirebaseFirestore.getInstance().collection("ChatUserCluster")
-        chatUserClusterCollection.document(currentUserUid).update("connectedUsers",FieldValue.arrayUnion(selectedUserUid))
-            .addOnSuccessListener {
-                Log.d("propertyInfoActivity","User $selectedUserUid added to the following $currentUserUid cluster successfully.")
-                chatUserClusterCollection.document(selectedUserUid).update("connectedUsers",FieldValue.arrayUnion(currentUserUid))
-                    .addOnSuccessListener { Log.d("propertyInfoActivity","User $currentUserUid added to the following $selectedUserUid cluster successfully.")
-                    }.addOnFailureListener{
-                        exception->
-                            Log.e("propertyInfoActivity","Errorr in adding user to the chat cluster",exception)
-                    }
-            }
-            .addOnFailureListener{
-                exception ->
-                Log.e("propertyInfoActivity","Error in adding user to the chat cluster",exception)
-            }
-    }
 
 
 
