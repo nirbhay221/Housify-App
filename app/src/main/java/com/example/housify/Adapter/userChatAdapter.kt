3389542fd0075116a7db4a2ae8b980f91103445f
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2023-12-19 16:06:56 -0500

    Timestamp Added.

--- app/src/main/java/com/example/housify/Adapter/userChatAdapter.kt
+++ app/src/main/java/com/example/housify/Adapter/userChatAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.housify.Adapter
 
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.housify.Models.chatModel
 import com.example.housify.R
+import com.google.firebase.Timestamp
+import java.util.Locale
 
 class userChatAdapter(private val chatList: List<chatModel>) : RecyclerView.Adapter<userChatAdapter.ChatViewHolder>() {
 
@@ -20,7 +23,7 @@ class userChatAdapter(private val chatList: List<chatModel>) : RecyclerView.Adap
         val chatModel = chatList[position]
 
         holder.textMessageForUser.text = chatModel.message
-        holder.textMessageTimestamp.text = chatModel.timestamp.toString()
+        holder.textMessageTimestamp.text = chatModel.timestamp?.let { formatTimestamp(it) }
 
     }
 
@@ -31,5 +34,10 @@ class userChatAdapter(private val chatList: List<chatModel>) : RecyclerView.Adap
     class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textMessageForUser: TextView = itemView.findViewById(R.id.textMessageForUser)
         val textMessageTimestamp: TextView = itemView.findViewById(R.id.textMessageTimestamp)
+    }
+    private fun formatTimestamp(timestamp: Timestamp): String {
+        val date = timestamp.toDate()
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault())
+        return dateFormat.format(date)
     }
 }
